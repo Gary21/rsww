27e49c9b07b03739449cfaaed6d72385956aaf34
rico <h.nacmer@gmail.com>
2024-04-11 22:40:52 +0200

    PoC

---
+++ transport-service/TransportService/Entities/Transport.cs
@@ -0,0 +1,37 @@
+﻿using MessagePack;
+
+namespace TransportService.Entities
+{
+    [MessagePackObject]
+    public class Transport
+    {
+        [Key(0)]
+        public string Id { get; set; } //long?
+        [Key(1)]
+        public string Type { get; set; }
+        [Key(2)]
+        public DateTime DepartureDate { get; set; }
+        [Key(3)]
+        public DateTime ArrivalDate { get; set; }
+        [Key(4)]
+        public string Destination { get; set; }
+        [Key(5)]
+        public string Origin { get; set; }
+        [Key(6)]
+        public int SeatsNumber { get; set; }
+        [Key(7)]
+        public int SeatsTaken { get; set; }
+        [Key(8)]
+        public decimal PricePerTicket { get; set; }
+
+    }
+
+    public enum TransportType
+    {
+        Plane,Bus,Ship
+    }
+
+    public class TransportDTO { 
+    
+    }
+}
